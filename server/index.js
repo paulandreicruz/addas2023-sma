56d6740cc07a40941929e1f5d143533a1c8f6d19
unknown <cruz.paulandrei99@gmail.com>
2023-06-07 20:18:01 +0800

    added socket io connections

--- server/index.js
+++ server/index.js
@@ -1,21 +1,30 @@
-//entry point
+//Entry point
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
+const app = express();
+
+//dotenv configuration
 dotenv.config();
 
-//Cloudinary Configuration
+//Creating http server using socket io
+const httpServer = createServer(app).listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
+const io = new Server(httpServer);
+
+// Cloudinary Configuration
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-const app = express();
-
-//Mongodb Database Connection
+// Mongodb Database Connection
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -23,9 +32,3 @@ mongoose
   })
   .then(() => console.log("Database connected"))
   .catch((err) => console.log("Database Error => ", err));
-
-//port connection
-const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
