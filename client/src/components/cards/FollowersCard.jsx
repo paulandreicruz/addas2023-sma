02455562375f40594c3d3c1ad7465246e851cd10
unknown <cruz.paulandrei99@gmail.com>
2023-07-13 00:17:26 +0800

    mock data who to follow

--- client/src/components/cards/FollowersCard.jsx
+++ client/src/components/cards/FollowersCard.jsx
@@ -1,8 +1,22 @@
+//
+import { MockFollowers } from "../../data/followers/mockData";
 
 export const FollowersCard = () => {
   return (
-    <div className="mx-auto">
-        WHO TO FOLLOW PANEL
+    <div className="">
+      <span>
+        Who to follow
+      </span>
+      {MockFollowers.map((follower, index) => {
+        return (
+          <div className="mt-2" key={index}>
+            <div className="md:flex mx-auto py-2 px-4 gap-3">
+              <span>{follower.name}</span>
+              <span>{follower.username}</span>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
--- client/src/data/followers/mockData.js
+++ client/src/data/followers/mockData.js
@@ -1,6 +1,21 @@
-export const FollowerCard = [
+export const MockFollowers = [
   {
     name: "Andrei Cruz",
+    username: "revlonsemaj",
+    img: "Asd",
+  },
+  {
+    name: "Andrei Jebs",
+    username: "revlonsemaj",
+    img: "Asd",
+  },
+  {
+    name: "Andrei Jaros",
+    username: "revlonsemaj",
+    img: "Asd",
+  },
+  {
+    name: "Andrei jeb",
     username: "revlonsemaj",
     img: "Asd",
   },
