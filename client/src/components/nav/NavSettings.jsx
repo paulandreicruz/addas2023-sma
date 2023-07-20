d15f853e6134dcb8227d5ab6aee82c0c228686ee
paulandreicruz <cruz.paulandrei99@gmail.com>
2023-07-20 17:28:53 +0800

    scroll icon added

--- client/src/components/nav/NavSettings.jsx
+++ client/src/components/nav/NavSettings.jsx
@@ -2,22 +2,25 @@
 import { IoSettingsOutline } from "react-icons/io5";
 import { FiMessageSquare } from "react-icons/fi";
 import { IoNotificationsOutline } from "react-icons/io5";
+import {BsArrowUpSquare} from "react-icons/bs"
 //
 
 export const NavSettings = () => {
   return (
-    <div className="w-[350px] text-center">
-      <div className="md:flex justify-between mr-7">
+    <div className="w-[350px]">
+      <div className="md:flex gap-11 items-center">
         <span>
-            UP SCROLL
+            <button className="btn btn-outline">
+                <BsArrowUpSquare size={27} />
+            </button>
         </span>
-        <span>
+        <span className="hover:cursor-pointer">
           <IoNotificationsOutline size={28} />
         </span>
-        <span>
+        <span className="hover:cursor-pointer">
           <FiMessageSquare size={28} />
         </span>
-        <span>
+        <span className="hover:cursor-pointer">
           <IoSettingsOutline size={28} />
         </span>
       </div>
